e04796ec3ac1af0a109aa53129acbd13f69b95e0
JUSTmang11 <peelut48@gmail.com>
2024-08-04 10:13:39 +0700

    configerfix

--- libs/config.js
+++ libs/config.js
@@ -1,8 +1,9 @@
 import { initializeApp, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import fs from 'fs'
+// import serviceAccount from '../configer.json' with {type:'json'}
+const serviceAccount = JSON.parse(fs.readFileSync('../configer.json', 'utf8'))
 
-const serviceAccount = JSON.parse(fs.readFileSync('../../NodeHostlearning/configer.json', 'utf8'))
 initializeApp({
     credential: cert(serviceAccount)
 })
