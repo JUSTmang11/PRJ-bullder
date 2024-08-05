7ed8c65961ec8923b932ca1b2d324050204319b3
JUSTmang11 <peelut48@gmail.com>
2024-08-05 18:59:07 +0700

    fixedV-1.2

--- route/profile.js
+++ route/profile.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import db from '../libs/config.js'
+// import condb from '../libs/conndb.js'
 const router = express.Router()
 
 async function Getdata(dataname){
@@ -9,22 +10,29 @@ async function Getdata(dataname){
     return data_row
 }
 
+// async function Getdata(dataname){
+//     const data_row = await condb.collection(dataname).get().then((snapshot)=>{
+//         return snapshot.docs.map(doc=> doc.data())
+//     })
+//     return data_row
+// }
+
 router.get('/',(req,res)=>{
     res.render('profile')
 })
 router.get('/test',async (req,res)=>{
-    const user = await Getdata('user')
-    if(user){
-        res.render('test.ejs',{
-            user:user,
-            status:true
-        })
-        console.log(user)
-    }else{
-        res.render('test.ejs',{
-            status:false
-        })
-    }
+    try{
+        const user = await Getdata('user')
+        if(user){
+            res.render('test.ejs',{
+                user:user,
+                status:true
+            })
+            console.log(user)
+        }
+    }catch(e){
+        console.log("Error: "+e)
+    }  
 })
 
 export default router
